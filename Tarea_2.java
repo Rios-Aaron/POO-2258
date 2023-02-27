94c0ac7e1184c54deb57ac61fb51eb928776e672
Aaron Rios <aaronrios85@aragon.unam.mx>
2023-02-27 05:08:59 -0600

    Tarea 2

---
+++ Tarea_2.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System. in);
+        System.out.println("Programa de validacion");
+        String caracteres = teclado.nextLine();
+        char uno = caracteres.charAt(0);
+        int decimal1 = (int) uno;
+        char dos = caracteres.charAt(1);
+        int decimal2 = (int) dos;
+        char tres = caracteres.charAt(2);
+        int decimal3 = (int) tres;
+        if (decimal1+1==decimal2 & decimal2+1==decimal3){
+            System.out.println("Cadena" +caracteres +"Es consecutiva");
+        }
+        else {
+            System.out.println("Cadena" +caracteres+ "No es consecutiva");
+        }
+    }
+}
