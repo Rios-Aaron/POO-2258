ff0bb769c84c372427fedc692b54510f876e7708
Aaron Rios <aaronrios85@aragon.unam.mx>
2023-02-27 05:15:25 -0600

    Tareas

--- Tarea_1.java
+++ Tarea_1.java
@@ -1,12 +1,12 @@
-importar java.util.Scanner;
-principal de clase pública{
-    public static void main(String[] args){
-        Scanner teclado=nuevo Scanner(System.in);
-        System.out.println("Introduce tu Nombre, Profesión y País: ");
-        String entrada=teclado.nextLine();
-        String[]salida=entrada.split(" ");
-        System.out.println("Nombre:"+salida[0].toUpperCase());
-        System.out.println("Profesión:"+salida[1].toUpperCase());
-        System.out.println("País:"+salida[2].toUpperCase());
-        }
-        }
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Nombre, Profesión, País:");
+        String entrada = teclado.nextLine();
+        String[] salida = entrada.split(" ");
+        System.out.println("Nombre:" + salida[0].toUpperCase());
+        System.out.println("Profesión:" + salida[1].toUpperCase());
+        System.out.println("País:" + salida[2].toUpperCase());
+    }
+}
